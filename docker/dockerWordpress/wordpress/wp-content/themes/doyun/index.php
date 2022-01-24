eeefd0ab736eadf080eea1bc55ea4d71a719fe34
Doyun <doyun@talktomeinkorean.com>
2022-01-24 12:39:43 +0900

    feature: newtodo functions 세팅 마무리

--- docker/dockerWordpress/wordpress/wp-content/themes/doyun/index.php
+++ docker/dockerWordpress/wordpress/wp-content/themes/doyun/index.php
@@ -1,5 +1,9 @@
-<?php 
-
+<?php
+    $todos = [];
+    if(file_exists('todo.json')) {
+        $json = file_get_contents('todo.json');
+        $todos = json_decode($json, true);
+    }
  ?>
 
  <!DOCTYPE html>
@@ -15,5 +19,13 @@
          <input type="text" name="todo_name" placeholder='Enter your todo'>
          <button>New Todo</button>
      </form>
+
+     <?php foreach($todos as $todoName => $todo): ?>
+        <div>
+            <input type="checkbox" <?php echo $todo['completed'] ? 'checked': '' ?>>
+            <?php echo $todoName ?>
+            <button>Delete</button>
+        </div>
+    <?php endforeach; ?>
  </body>
  </html>
